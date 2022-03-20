3e8cb6f1ba805c9fd1ec0756a6860d3d3bb449b6
Dominik Smatana <taidana@kage.sk>
2022-03-20 12:37:28 +0100

    FK uptime --> realmlist
    Added a foreign key: Uptime.RealmID --> RealmList.ID
    
    For testing purposes.

--- Integrity test/FK_test_for_Rel 22_1_1.sql
+++ Integrity test/FK_test_for_Rel 22_1_1.sql
@@ -9,3 +9,8 @@ UPDATE `account` SET `active_realm_id` = NULL WHERE `active_realm_id` = 0;
 ALTER TABLE `account`
   ADD CONSTRAINT `fk_account_realmlist` FOREIGN KEY (`active_realm_id`) REFERENCES `realmlist` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
+
+/* Uptime.RealmID --> RealmList.ID */
+
+ALTER TABLE `uptime`
+  ADD CONSTRAINT `fk_uptime_realmlist` FOREIGN KEY (`realmid`) REFERENCES `realmlist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
